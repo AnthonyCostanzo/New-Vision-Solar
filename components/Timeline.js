b661443dbe49881b2d754453965114317ce3dce8
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-06-23 16:59:27 -0400

    space

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 const TimeLine = () => {
   return (
-    <div className="relative h-96 md:w-8/12 m-auto md:mb-2 ">
+    <div className="relative h-96 md:w-8/12 md:m-auto m-2 mb-10 md:mt-0 mt-10 md:mb-2 ">
       <div className="max-w-min md:m-2">
         <Image
           src="/images/timeline.jpeg"
           alt="p"
-          className="md:rounded-md"
+          className="rounded-lg"
           layout="fill"
         />
       </div>

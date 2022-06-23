273be5c2ddf237895c5e4bab1fb2ec5878bc0751
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-06-23 18:37:23 -0400

    hero

--- components/Valuation.js
+++ components/Valuation.js
@@ -4,7 +4,7 @@ const Valuation = () => (
     className="h-[20rem] md:h-[38rem] lg:h-[45rem] w-full relative "
     id="solar"
   >
-    <div className="flex bg-red-50 h-full w-full justify-center">
+    <div className="flex  bg-red-50 h-full w-full justify-center">
       <Image
         src={`/images/bs.png`}
         className="shadow-sm shadow-green-200"

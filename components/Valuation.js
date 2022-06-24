d61a20ac9c931b5263770639e035301ddb12ef0c
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-06-23 23:48:43 -0400

    f

--- components/Valuation.js
+++ components/Valuation.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 const Valuation = () => (
   <div className="w-full" id="solar">
-    <div className="justify-center mx-2">
+    <div className="justify-center md:mx-2">
       <Image
         src={`/images/bs.png`}
-        className="shadow-sm rounded-md shadow-green-200"
+        className="shadow-sm md:rounded-lg shadow-green-200"
         alt="a"
         height={300}
         width={500}

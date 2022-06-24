ca0337242d880793978e9264896409ea18199c8c
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-06-24 17:12:18 -0400

    a

--- components/Problems.js
+++ components/Problems.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const Problems = () => (
   <>
-    <div className="h-48 relative lg:py-32">
+    <div className="h-48 relative lg:py-40">
       <div className="bg-red-200 absolute"></div>
       <Image
         src="/images/divide.png"
@@ -11,7 +11,7 @@ const Problems = () => (
         objectFit="cover"
       />
     </div>
-    <div className="md:mx-2 rounded-md grid md:grid-cols-2 gap-5 pb-5">
+    <div className="md:mx-2 lg:mt-20 rounded-md grid md:grid-cols-2 gap-5 pb-5">
       <div className="">
         <Image
           src="/images/Problems.png"
@@ -22,11 +22,12 @@ const Problems = () => (
           layout="responsive"
         />
       </div>
+      <div className="mx-0 md:mx-0 h-80 md:pt-[30rem] lg:pt-[50rem] md:h-96 bg-cover md:bg-center bg-no-repeat lg:h-96 bg-[url('/images/r.png')] relative col-span-2 order-3"></div>
       <div className="">
         <Image
           src="/images/prod.png"
           alt="p"
-          className="md:rounded-lg"
+          className=""
           height={200}
           width={250}
           layout="responsive"

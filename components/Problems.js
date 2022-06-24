f1e63ccdd6f63858d6126f779558ebfe21f0fae0
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-06-23 23:32:32 -0400

    1

--- components/Problems.js
+++ components/Problems.js
@@ -1,23 +1,23 @@
 import Image from "next/image";
 const Problems = () => (
-  <div className="my-14 rounded-md mx-5 grid md:grid-cols-2 gap-5 pt-5 md:gap-10 pb-5">
-    <div className="p-5">
+  <div className="mt-20 rounded-md grid md:grid-cols-2 gap-20 lg:mx-10 pt-5 pb-20">
+    <div className="px-2">
       <Image
         src="/images/Problems.png"
         alt="p"
         className="rounded-md"
-        height={100}
-        width={180}
+        height={200}
+        width={250}
         layout="responsive"
       />
     </div>
-    <div className="p-4">
+    <div className="px-2">
       <Image
         src="/images/prod.png"
         alt="p"
         className="rounded-md"
-        height={100}
-        width={180}
+        height={200}
+        width={250}
         layout="responsive"
       />
     </div>{" "}
--- components/Valuation.js
+++ components/Valuation.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 const Valuation = () => (
   <div className="w-full" id="solar">
-    <div className="justify-center mx-5">
+    <div className="justify-center mx-2">
       <Image
         src={`/images/bs.png`}
-        className="shadow-sm shadow-green-200"
+        className="shadow-sm rounded-md shadow-green-200"
         alt="a"
         height={300}
         width={500}

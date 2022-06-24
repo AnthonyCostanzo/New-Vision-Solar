423b602fcb3b268655c7e238b3d8f7b0ba50b22d
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-06-24 17:44:14 -0400

    a

--- components/Timeline.js
+++ components/Timeline.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 const TimeLine = () => {
   return (
     <div className="flex">
-      <div className="relative mx-2 my-5 w-full h-[22rem] rounded-lg md:h-[28rem] lg:h-[48rem] ">
+      <div className="relative mx-2 w-full h-[22rem] rounded-lg md:h-[28rem] lg:h-[48rem] ">
         <div className=" w-full rounded-lg">
           <Image
             src="/images/time.png"

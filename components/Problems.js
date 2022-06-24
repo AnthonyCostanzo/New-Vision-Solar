d738a7bcffc87555b7b9c9d0b10f49f3f040d51c
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-06-23 23:46:04 -0400

    f

--- components/Problems.js
+++ components/Problems.js
@@ -1,21 +1,21 @@
 import Image from "next/image";
 const Problems = () => (
   <div className=" rounded-md grid md:grid-cols-2 gap-5 lg:mx-10 pt-5 pb-5">
-    <div className="px-2">
+    <div className="">
       <Image
         src="/images/Problems.png"
         alt="p"
-        className="rounded-md"
+        className="md:rounded-lg"
         height={200}
         width={250}
         layout="responsive"
       />
     </div>
-    <div className="px-2">
+    <div className="">
       <Image
         src="/images/prod.png"
         alt="p"
-        className="rounded-md"
+        className="md:rounded-lg"
         height={200}
         width={250}
         layout="responsive"
--- components/Timeline.js
+++ components/Timeline.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 const TimeLine = () => {
   return (
     <div className="flex">
-      <div className="relative w-full mb-16 mx-2 md:mx-10 h-[22rem] md:h-[28rem] lg:h-[43rem] ">
+      <div className="relative w-full mb-16  md:mx-10 h-[22rem] md:h-[28rem] lg:h-[43rem] ">
         <div className=" w-full">
           <Image
             src="/images/time.png"
             alt="p"
-            className="rounded-lg"
+            className="md:rounded-lg"
             layout="fill"
           />
         </div>

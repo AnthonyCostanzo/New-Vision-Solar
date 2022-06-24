9caead6d5d054f6f033c52a80f06cf3f97ff5374
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-06-24 18:13:59 -0400

    lead

--- components/Timeline.js
+++ components/Timeline.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 const TimeLine = () => {
   return (
     <div className="flex">
-      <div className="relative mt-[-2.5rem] mx-2 w-full h-[22rem] rounded-lg md:h-[28rem] lg:h-[48rem] ">
+      <div className="relative mt-[-2.5rem] lg:mb-6 lg:mt-[2.5rem] mx-2 w-full h-[22rem] rounded-lg md:h-[28rem] lg:h-[48rem] ">
         <div className=" w-full rounded-lg">
           <Image
             src="/images/time.png"

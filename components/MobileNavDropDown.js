1560197e033e50327ceafdf4f32306c711bcacf0
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-06-23 22:43:23 -0400

    a

--- components/MobileNavDropDown.js
+++ components/MobileNavDropDown.js
@@ -2,11 +2,11 @@ const MobileNavDropdown = ({ toggleNav }) => {
   return (
     <button
       onClick={toggleNav}
-      className="bg-gray-100 h-8 rounded-md w-10 flex flex-col items-center md:hidden"
+      className="bg-gray-200 h-8 rounded-md w-10 flex flex-col items-center md:hidden"
     >
       <div className=" border-[2px] border-green-500 mb-[3px] w-8 mt-2"></div>
-      <div className=" border-[2px] border-sky-600 mb-[3px] w-8"></div>
-      <div className=" border-[2px] border-green-700 mb-[3px] w-8"></div>
+      <div className=" border-[2px] border-blue-400 mb-[3px] w-8"></div>
+      <div className=" border-[2px] border-sky-700 mb-[3px] w-8"></div>
     </button>
   );
 };

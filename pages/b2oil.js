a65fe1544439f5fa745023a3b7b59c427e2992e1
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-08-09 13:53:54 -0400

    b2 oil

--- pages/b2oil.js
+++ pages/b2oil.js
@@ -16,33 +16,40 @@ const B2Oil = () => {
             ></Image>
           </div>
         </div>
-        <div className="md:flex order-1 text-gray-200 bg-gradient-to-r from-[#db5c08] to-[#f67a22b4] p-10  shadow-sm shadow-gray-300">
-          <div className="flex justify-center py-7 md:py-0  md:w-1/4  ">
-            <div className="relative w-56 h-40 md:w-40 md:h-48">
-              <Image
-                alt="B2 Oil logo"
-                src="/images/b2logo.png"
-                layout="fill"
-              ></Image>
+        <div className=" text-gray-200 md:order-2 bg-gradient-to-r from-[#db5c08] to-[#f67a22b4] p-10  shadow-sm shadow-gray-300">
+          <div className="md:flex">
+            <div className="flex justify-center py-7 md:py-0  md:w-1/4  ">
+              <div className="relative w-56 h-40 md:w-40 md:h-48">
+                <Image
+                  alt="B2 Oil logo"
+                  src="/images/b2logo.png"
+                  layout="fill"
+                ></Image>
+              </div>
             </div>
+            <p className="text-[20px] md:mt-3 md:ml-3 text-gray-7000 font-[Times]">
+              b2 biOil has been created to address the world’s growing
+              “lubricant” concerns regarding motor oil’s effects on the
+              environment (whether during oil production and/or during the oil’s
+              ‘end-of-life’ improper disposal) with an eco-friendly offering
+              that satisfies growing consumer demands while meeting the needs of
+              on-going government requirements that mandate the procurement of
+              domestically sourced bio-derived biodegradable products. Our
+              eco-friendly offering, b2 biOil “biodegradable & bio-based” full
+              synthetic supreme motor oil, is properly positioned to satisfy the
+              above needs as well as providing superior performance motor oil
+              that exceeds the demands of the industry. It is API SN and ILSAC
+              G-5 certified, satisfies the requirements to be included within
+              the USDA BioPreferred Procurement Program, biodegrades up to 60%
+              within 28 days and is parity priced with other similar
+              claiming/performing full synthetic motor oils.
+            </p>
           </div>
-          <p className="text-[20px] mt-3 ml-3 text-gray-7000 font-[Times]">
-            b2 biOil has been created to address the world’s growing “lubricant”
-            concerns regarding motor oil’s effects on the environment (whether
-            during oil production and/or during the oil’s ‘end-of-life’ improper
-            disposal) with an eco-friendly offering that satisfies growing
-            consumer demands while meeting the needs of on-going government
-            requirements that mandate the procurement of domestically sourced
-            bio-derived biodegradable products. Our eco-friendly offering, b2
-            biOil “biodegradable & bio-based” full synthetic supreme motor oil,
-            is properly positioned to satisfy the above needs as well as
-            providing superior performance motor oil that exceeds the demands of
-            the industry. It is API SN and ILSAC G-5 certified, satisfies the
-            requirements to be included within the USDA BioPreferred Procurement
-            Program, biodegrades up to 60% within 28 days and is parity priced
-            with other similar claiming/performing full synthetic motor oils.
-          </p>
+          <button className="w-36 hover:font-bold border-[1.2px] shadow-sm shadow-orange-200 bg-white rounded-full h-14 mt-2 text-orange-400">
+            <a href="http://www.b2oil.com/sneak-peak.html">LEARN MORE</a>
+          </button>
         </div>
+
         <div className=" order-3 flex flex-col md:flex-row bg-gray-500 p-5">
           <div className="relative m-auto h-48 w-48 md:h-[30vh] md:w-9/12 lg:w-5/12 mt-5 ">
             <Image
